4e516641e9744ac7cd232beee8b4b7bd5d315803
David Kovacs <david.kovacs@ngena.net>
2022-06-20 15:18:20 +0200

    added type selection

--- game.py
+++ game.py
@@ -1,10 +1,18 @@
 from dataclasses import dataclass
 from typing import List
 
-@dataclass
-class Robot:
-    name : str
-    hp : int
-    energy : int
-    weapons : List[object]
-    
+from classes import Weapons, RobotTypes
+
+weapons = Weapons()
+robot_types = RobotTypes()
+
+print('\nWELCOME TO THE CYBER PIT!')
+print('Choose your robot type:')
+print(robot_types.list_all())
+
+type = input('What will it be?  ').lower()
+while type not in robot_types.__dict__:
+    type = input('Choose from existing types listed above.  ').lower()
+
+print('Good choice.')
+
