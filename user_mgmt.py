01891d5ea19845b63c302fa0d89ea533f72e4497
David Kovacs <david.kovacs@ngena.net>
2022-09-14 07:55:49 +0200

    changed user_prompt(), now load_user(), improved username input, minor housekeeping

--- user_mgmt.py
+++ user_mgmt.py
@@ -1,23 +1,40 @@
 
 from getpass import getpass
-from db import DBHandler
 import bcrypt
+
+from db import DBHandler
+from dcs import User
 
 class UserHandler:
     def __init__(self) -> None:
         self.db_handle = DBHandler()
 
+    def load_user(self):
+        username = input('If you have a user, enter you username, otherwise press Enter to create a user:\n')
+        if username:
+            username, new = self.existing_user(username)
+        else:
+            username = self.new_user()
+            new = True
+        user = User(username)
+        self.db_handle.wq()
+        return user, new
+
     def new_user(self):
         username = input('Enter username for new user: ')
+        while not username:
+            username = input('Username invalid, try again: ')
         while self.db_handle.user_exists('users', username):
-                username = input(f'\nUser with name {username} already exists.\n'
-                                  'Choose a different username: ')
-        password = self.set_password()
+            username = input(f'\nUser with name {username} already exists.\n'
+                                'Choose a different username: ')
+        password = self._set_password()
         self.db_handle.create_user('users', username, password)
         return username
 
     def existing_user(self, name):
         while not self.db_handle.user_exists('users', name):
+            if name == '':
+                return self.new_user(), True
             name = input(f'User with name "{name}" does not exist.\n' 
                           'Enter your username: ')
         access = False
@@ -25,28 +42,19 @@ class UserHandler:
             access = bcrypt.checkpw(getpass().encode('utf-8'), 
                                     self.db_handle.get_pwdhash('users', name))
             if not access:
-                print('Wrong password!')
+                print(':-(')
+        print('<-- ACCESS GRANTED -->')
+        return name, False
 
-    def set_password(self):
-        pwd, confirm = "0", "1"
+    def _set_password(self):
         pwd = str(getpass('Enter password for new user: '))
         confirm = str(getpass('Confirm the password for new user: '))
         if pwd != confirm:
             print('Passwords does not match!\n')
-            self.set_password()
-        del confirm
-        return self.get_hash(pwd)
+            self._set_password()
+        return self._get_hash(pwd)
 
-    def user_prompt(self):
-        username = input('If you have a user, enter you username, otherwise press Enter to create a user:\n')
-        if username:
-            self.existing_user(username)
-        else:
-            username = self.new_user()
-        self.db_handle.wq()
-        return username
-
-    def get_hash(self, string: str):
+    def _get_hash(self, string: str):
         bytes = string.encode('utf-8')
         salt = bcrypt.gensalt()
         return bcrypt.hashpw(bytes, salt)
