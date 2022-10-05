38ef597c7bf66ea439ba2dfdb6919268c7e2de70
David Kovacs <david.kovacs@ngena.net>
2022-10-05 10:13:33 +0200

    dependency injection

--- intro.py
+++ intro.py
@@ -1,13 +1,18 @@
 from users import UserHandler, new_user_sequence
+from db import DBHandler
 from base import clear_console
+from time import sleep
 
 
-def starting_sequence():
+def starting_sequence(user_handler: UserHandler):
     print('\nWELCOME TO THE CYBER PIT!')
-    user, new = UserHandler().load_user()
+    user, new = user_handler.user_login()
     clear_console()
-    print('\n--------------------------')
-    print(f'Welcome {user.name}!')
     if new:
         new_user_sequence(user)
-    print(f'Your current balance is {user.balance} bitcoins.')
+        clear_console()
+    print('\n--------------------------')
+    print(f'Welcome {user.name}!')
+    sleep(1)
+    print(f'Your current robot: \n{user.robot}')
+    return user
