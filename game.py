5baab0fc59d3f18fd27ca244db290fa166b5d98a
David Kovacs <david.kovacs@ngena.net>
2022-10-05 10:12:38 +0200

    moved user_handler instantiation to main, added new steps in game flow

--- game.py
+++ game.py
@@ -1,7 +1,19 @@
 import intro
+from users import UserHandler
+from db import DBHandler
 
 if __name__ == '__main__':
-    intro.starting_sequence()
+    user_handler = UserHandler(DBHandler())
+    user = intro.starting_sequence(user_handler)
+    while True:
+        action = input('If you wish to buy a new robot, type "shop".\n'
+                     'Note - new robot will replace the current one.\n'
+                     'If you are happy with your current robot, type "continue":\n')
+        if action == 'shop':
+            user.purchase_robot()
+            break
+        if action == 'continue':
+            break
 
 
 '''
