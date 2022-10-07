e8f5f505213c5d125bb8c2a0c3a1eb1d918a769c
David Kovacs <david.kovacs@ngena.net>
2022-10-07 21:39:47 +0200

    refactor - moved the game flow to Game class

--- game.py
+++ game.py
@@ -1,11 +1,35 @@
-from src.users import UserManager, PwdManager
+from src.users import UserManager, PwdManager, User
 from src.db import DBHandler
 from src.menu import MainMenu
+from src.utils import clear_console, theme
 
-if __name__ == '__main__':
+class Game:
+    def __init__(self, user_manager: UserManager, main_menu: MainMenu) -> None:
+        self.user_manager = user_manager
+        self.main_menu = main_menu
+
+    def user_init(self, user: User):
+        self.user = user
+
+    def starting_sequence(self):
+        '''
+        Starting sequence for user login/creation.
+        Returns user object
+        '''
+        self.user_init(self.user_manager.read_username())
+        clear_console()
+        theme()
+    
+    def run(self):
+        self.starting_sequence()
+        self.main_menu.present_menu(self.user)
+
+def main():
     try:
-        main_menu = MainMenu()
-        user = main_menu.starting_sequence(UserManager(DBHandler(), PwdManager()))
-        main_menu.present_menu(user)
+        game = Game(UserManager(DBHandler(), PwdManager()), MainMenu())
+        game.run()
     except KeyboardInterrupt:
         print('\nYou pressed a magic combination of keys (ctrl + c), quitting the game...')
+
+if __name__ == '__main__':
+    main()
