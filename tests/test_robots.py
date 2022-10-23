c410cd77fc18c45bd96612e430f2757fd9cb872b
David Kovacs <david.kovacs@ngena.net>
2022-10-23 15:32:17 +0200

    started with robots tests

---
+++ tests/test_robots.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src import robots
+
+
+def test_weapons_get_energy():
+    '''
+    Testing weapons get energy method with various inputs.
+    '''
+    assert robots.Weapons.get_energy('laser') == 6
+    assert robots.Weapons.get_energy('spike') == 2
+    assert robots.Weapons.get_energy('plasma_gun') == 12
+
+    with pytest.raises(KeyError):
+        assert robots.Weapons.get_energy('')
+        assert robots.Weapons.get_energy('nonsense')
+
+    with pytest.raises(AttributeError):
+        assert robots.Weapons.get_energy(5)
+        assert robots.Weapons.get_energy([0,1])
